d2e257622b0d85a4d8aff9204677998d6aa08c20
isacgpaz <isacgomesp@gmail.com>
2021-08-26 14:04:17 -0300

    Initial setup

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import { app } from './app';
+
+mongoose.connect(String(process.env.DATABASE_URL), {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, '⚠ Connection error.'));
+connection.once('open', () => {
+  console.log('🍃 Connected to MongoDB.');
+});
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log('🔥 Server is now running.');
+});
