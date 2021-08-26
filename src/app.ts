abf417e9756d222e978c614209f3c8453680c9d7
isacgpaz <isacgomesp@gmail.com>
2021-08-26 14:32:19 -0300

    Set routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 
+import { basicRouter, userRouter } from './routes';
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/', basicRouter);
+app.use('/users', userRouter);
 
 export { app }
