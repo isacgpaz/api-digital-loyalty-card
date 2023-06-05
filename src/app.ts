1f1fbfe9e47cf14c8b465a33f9a7a07433deed07
Isac Gomes <isacgomesp@gmail.com>
2023-06-05 16:07:44 -0300

    chore: organize imports

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import { basicRouter, userRouter, adminRouter } from './routes';
+import { basicRouter, userRouter, adminRouter } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/', basicRouter);
-app.use('/users', userRouter);
-app.use('/admins', adminRouter);
+app.use("/", basicRouter);
+app.use("/users", userRouter);
+app.use("/admins", adminRouter);
 
-export { app }
+export { app };
